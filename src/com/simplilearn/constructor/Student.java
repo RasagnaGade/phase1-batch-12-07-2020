5a5b5fec875220c3eaa472ee21a3c818bc4c327e
RasagnaGade <rasagnagade30@gmail.com>
2020-12-24 17:51:44 +0000

    mystatic

--- src/com/simplilearn/constructor/Student.java
+++ src/com/simplilearn/constructor/Student.java
@@ -1,5 +1,26 @@
 package com.simplilearn.constructor;
 
 public class Student {
+	
+	
+	public static void main(String[] args) {
+		Students std = new Students();
+		std.showStudent(12, "andy", 100);
+	}
+}
+
+    class Students 
+   {
+	int no;
+	String name;
+	int marks;
+
+	 void showStudent(int no, String name, int marks) {
+		this.no = no;
+		this.name = name;
+		this.marks = marks;
+		System.out.println("student with " + no + " and name " + name +" scored " +marks) ;
+
+	}
 
 }
--- src/com/simplilearn/mystatic/Employee.java
+++ src/com/simplilearn/mystatic/Employee.java
@@ -1,5 +1,34 @@
 package com.simplilearn.mystatic;
 
 public class Employee {
+	int id;
+	static String name="andy";
+	
+	public static void showEmployee()
+	{
+		System.out.println("employee name:: " +name);
+	}
+	
+	public void display() {
+		System.out.println("employee id:: " +id);
+	}
 
-}
+
+
+
+
+
+
+	public static void main(String[] args)
+	{
+		System.out.println("my name:: "+Employee.name);
+		Employee.showEmployee();
+		
+		Employee e=new Employee();
+		System.out.println("Employee id::" +e.id);
+		e.display();
+		
+	}
+	}
+
+
